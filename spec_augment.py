7e11025837791a6bfefa8c17376353528c0a4227
shelling203 <shelling203@gmail.com>
2019-04-25 10:48:07 +0900

    Update remark

--- spec_augment.py
+++ spec_augment.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from tensorflow.contrib.image.python.ops import sparse_image_warp
 import tensorflow as tf
 import numpy as np
 import librosa
@@ -21,11 +20,12 @@ import librosa.display
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-from tensorflow.python.framework import constant_op
 
-def spec_augment(input, time_warping_para, frequency_masking_para, time_masking_para, num_mask):
-  """Compute spec augmentation.
+def spec_augment(mel_spectrogram, time_warping_para, frequency_masking_para, time_masking_para, num_mask):
+  """Spec augmentation.
+  Related paper : https://arxiv.org/pdf/1904.08779.pdf
   The Parameters, "Augmentation parameters for policies", refer to the 'Tabel 1' in the paper.
+
   Args:
     input: Extracted mel-spectrogram, numpy array.
     time_warping_para: "Augmentation parameters for policies W", LibriSpeech is 80.
@@ -35,10 +35,16 @@ def spec_augment(input, time_warping_para, frequency_masking_para, time_masking_
   Returns:
     raw : warped and masked mel spectrogram.
   """
-  raw = input
-  ta = 128
+  tau = 128
 
-  ## todo : time warping
+  """(TO DO)Time warping
+  In paper Time warping written as follows. 'Given a log mel spectrogram with τ time steps,
+  we view it as an image where the time axis is horizontal and the frequency axis is vertical.
+  A random point along the horizontal line passing through the center of the image within the time steps (W, τ − W)
+  is to be warped either to the left or right by a distance w chosen from a uniform distribution
+  from 0 to the time warp parameter W along that line.'
+  In paper Using Tensorflow's 'sparse-image-warp'.
+  """
   # raw = raw.reshape([1, raw.shape[0], raw.shape[1], 1])
   # w = random.randint(0, time_warping_para)
   # warped = tf.contrib.image.sparse_image_warp(raw,
@@ -50,46 +56,48 @@ def spec_augment(input, time_warping_para, frequency_masking_para, time_masking_
   #                                             name='sparse_image_warp'
   #                                             )
 
-  # repeat number of mask lines
+  """ Masking line loop """
   for i in range(num_mask):
     """Frequency masking
     In paper Frequency masking written as follows. 'Frequency masking is applied so that f consecutive mel frequency
     channels [f0, f0 + f) are masked, where f is first chosen from a uniform distribution from 0 to the frequency mask parameter F,
     and f0 is chosen from [0, ν − f). ν is the number of mel frequency channels.'
+    In this code, ν was written with v. ands For choesing 'f' uniform distribution, I using random package.
     """
-    f = np.random.uniform(low=0.0, high = frequency_masking_para)
+    f = np.random.uniform(low=0.0, high=frequency_masking_para)
     f = int(f)
     v = 128  # Now hard coding but I will improve soon.
     f0 = random.randint(0, v - f)
-    raw[f0:f0+f, :] = 0
+    mel_spectrogram[f0:f0+f, :] = 0
 
 
     """Time masking
-    In paper Frequency masking written as follows. 'Time masking is applied so that t consecutive time steps
+    In paper Time masking written as follows. 'Time masking is applied so that t consecutive time steps
     [t0, t0 + t) are masked, where t is first chosen from a uniform distribution from 0 to the time mask parameter T,
     and t0 is chosen from [0, τ − t).'
+    In this code, τ(tau) was written with tau. and For choesing 't' uniform distribution, I using random package.
     """
-    t = np.random.uniform(low=0.0, high = time_masking_para)
+    t = np.random.uniform(low=0.0, high=time_masking_para)
     t = int(t)
-    t0 = random.randint(0, ta-t)
-    raw[:, t0:t0+t] = 0
+    t0 = random.randint(0, tau-t)
+    mel_spectrogram[:, t0:t0+t] = 0
 
-  return raw
+  return mel_spectrogram
 
 # First, we need to load sample audio file
-# we use one of the LibriSpeech data
+# For the test, I use one of the 'Libiri Speech' data.
 audio_path = "./data"
 audio_file = "./data/61-70968-0002.wav"
 y, sr = librosa.load(audio_file)
 
-# for extracting mel-spectrogram feature, we using LibSosa.
+# For extracting mel-spectrogram feature, I use 'LibSosa' python audio package.
 S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
 
 # we can see extracted mel-spectrogram just simple method using 'specshow'
 plt.figure(figsize=(10, 4))
 librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='mel', fmax=8000, x_axis='time')
 plt.colorbar(format='%+2.0f dB')
-plt.title('Mel spectrogram')
+plt.title('Mel Spectrogram')
 plt.tight_layout()
 plt.show()
 
@@ -103,7 +111,7 @@ masked_spec = spec_augment(S, time_warping_para=80,
 plt.figure(figsize=(10, 4))
 librosa.display.specshow(librosa.power_to_db(masked_spec, ref=np.max), y_axis='mel', fmax=8000, x_axis='time')
 plt.colorbar(format='%+2.0f dB')
-plt.title('Mel spectrogram MASKED')
+plt.title('Masked Mel Spectrogram')
 plt.tight_layout()
 plt.show()
 
