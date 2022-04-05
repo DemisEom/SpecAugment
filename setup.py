42eb807d64178b9c4e0bd9f8827d8256e1996b8a
Chanran Kim <seriousran@gmail.com>
2022-04-05 09:37:29 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-   name='SpecAugment',
+   name='specAugment',
    version='1.2.3',
    description='A implementation of "SpecAugment"',
-   url              = 'https://github.com/shelling203/SpecAugment',
+   url              = 'https://github.com/seriousran/SpecAugment',
    packages         = find_packages(exclude = ['docs', 'tests*']),
    install_requires=['tensorflow', 'librosa', 'matplotlib', 'torch'], #external packages as dependencies
 )
