bae55e1ca3db95c643a2e843c78473ee53f71831
Chanran Kim <seriousran@gmail.com>
2022-04-05 09:38:41 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='specAugment',
-   version='1.2.3',
+   version='1.3.0',
    description='A implementation of "SpecAugment"',
    url              = 'https://github.com/seriousran/SpecAugment',
    packages         = find_packages(exclude = ['docs', 'tests*']),
